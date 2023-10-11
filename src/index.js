72e455f5a4335d39b4e3f384881079e71aaca3a5
NO NAME <dimazver161@gmail.com>
2023-10-11 09:03:54 +0300

    modified:   package.json 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div className='app'>
-    <BrowserRouter basename={'/AddressTracker-TG-WebApp'}>
+    <BrowserRouter basename={'/'}>
       <App />
     </BrowserRouter>
   </div>
